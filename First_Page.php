6b2dc89b65255202296f07681ed9f4451adacfa4
kajshjkfjsbjcvbshueghfnsg <113829617+kajshjkfjsbjcvbshueghfnsg@users.noreply.github.com>
2023-10-01 12:59:08 +0530

    Initial commit

---
+++ First_Page.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="landing_page.css">
+</head>
+<body>
+    <!-- navigation bar -->
+    <div class="container red topBotomBordersOut">
+        <section>
+            <div class="part">
+                <a href="home">HOME</a>
+                <a href="brans">BRANDS</a>
+                <a href="sale">SELL</a>
+                <a href="contact">CONTACT</a>
+                <a href="about us">ABOUT US</a>
+                <p class="Logo">
+                    Tech Heaven
+                </p>
+                <input class="textbox" type="text" placeholder="What you are looking for?">
+                <p class="location">
+                    Location 
+                </p>
+                <div class="person">
+                    <img class="account" src="person.png" height="25px">
+                </div>
+                <div class="cart">
+                    <img src="cart.png" height="25px" class="Cart">
+                </div>
+            </div>
+        </section>
+      </div>
+      <div class="slide_show">
+        <section>
+        <div class="slide_show">
+            <div class="image-slideshow">
+            <div class="image fade">
+                <img src="washisng machine.jpg" alt="Mountain Top">
+            </div>        
+            <div class="image fade">
+                <img src="mobile phone.png" alt="Palm Trees">
+            </div>
+            .image fade
+        </div>
+            <script src="main.js"></script>
+        </div>
+        </section>
+      </div>
+      <script src="slide_show.js"></script>
+</body>
+</html>
